67803ff4001af0d4afadc84d333844676a9045db
BOURDEA_BREN <BOURDEA_BREN@CNU3299XCY.blue.ad.bentley.edu>
2015-04-20 23:41:58 -0400

    good luck

---
+++ MyoController/src/com/example/myocontroller/BackgroundService.java
@@ -0,0 +1,141 @@
+package com.example.myocontroller;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.Arm;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.Quaternion;
+import com.thalmic.myo.XDirection;
+
+public class BackgroundService extends Service {
+
+    // Classes that inherit from AbstractDeviceListener can be used to receive events from Myo devices.
+    // If you do not override an event, the default behavior is to do nothing.
+    private DeviceListener mListener = new AbstractDeviceListener() {
+
+        // onConnect() is called whenever a Myo has been connected.
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            // Set the text color of the text view to cyan when a Myo connects.
+            
+        }
+
+        // onDisconnect() is called whenever a Myo has been disconnected.
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            // Set the text color of the text view to red when a Myo disconnects.
+           
+        }
+
+        // onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
+        // arm. This lets Myo know which arm it's on and which way it's facing.
+        @Override
+        public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
+            
+        }
+
+        // onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
+        // it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
+        // when Myo is moved around on the arm.
+        @Override
+        public void onArmUnsync(Myo myo, long timestamp) {
+            
+        }
+
+        // onUnlock() is called whenever a synced Myo has been unlocked. Under the standard locking
+        // policy, that means poses will now be delivered to the listener.
+        @Override
+        public void onUnlock(Myo myo, long timestamp) {
+            
+        }
+
+        // onLock() is called whenever a synced Myo has been locked. Under the standard locking
+        // policy, that means poses will no longer be delivered to the listener.
+        @Override
+        public void onLock(Myo myo, long timestamp) {
+            
+        }
+
+        // onOrientationData() is called whenever a Myo provides its current orientation,
+        // represented as a quaternion.
+        @Override
+        public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
+            // Calculate Euler angles (roll, pitch, and yaw) from the quaternion.
+            float roll = (float) Math.toDegrees(Quaternion.roll(rotation));
+            float pitch = (float) Math.toDegrees(Quaternion.pitch(rotation));
+            float yaw = (float) Math.toDegrees(Quaternion.yaw(rotation));
+
+            // Adjust roll and pitch for the orientation of the Myo on the arm.
+            if (myo.getXDirection() == XDirection.TOWARD_ELBOW) {
+                roll *= -1;
+                pitch *= -1;
+            }
+
+            // Next, we apply a rotation to the text view using the roll, pitch, and yaw.
+            
+        }
+
+        // onPose() is called whenever a Myo provides a new pose.
+        @Override
+        public void onPose(Myo myo, long timestamp, Pose pose) {
+            // Handle the cases of the Pose enumeration, and change the text of the text view
+            // based on the pose we receive.
+            switch (pose) {
+                case UNKNOWN:
+                    
+                    break;
+                case REST:
+                case DOUBLE_TAP:
+                    int restTextId = R.string.hello_world;
+                    switch (myo.getArm()) {
+                        case LEFT:
+                            restTextId = R.string.arm_left;
+                            break;
+                        case RIGHT:
+                            restTextId = R.string.arm_right;
+                            break;
+                    }
+                    
+                    break;
+                case FIST:
+                    
+                    break;
+                case WAVE_IN:
+                    
+                    break;
+                case WAVE_OUT:
+                    
+                    break;
+                case FINGERS_SPREAD:
+                    
+                    break;
+            }
+
+            if (pose != Pose.UNKNOWN && pose != Pose.REST) {
+                // Tell the Myo to stay unlocked until told otherwise. We do that here so you can
+                // hold the poses without the Myo becoming locked.
+                myo.unlock(Myo.UnlockType.HOLD);
+
+                // Notify the Myo that the pose has resulted in an action, in this case changing
+                // the text on the screen. The Myo will vibrate.
+                myo.notifyUserAction();
+            } else {
+                // Tell the Myo to stay unlocked only for a short period. This allows the Myo to
+                // stay unlocked while poses are being performed, but lock after inactivity.
+                myo.unlock(Myo.UnlockType.TIMED);
+            }
+        }
+    };
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ MyoController/src/com/example/myocontroller/Configuration.java
@@ -0,0 +1,91 @@
+package com.example.myocontroller;
+
+import java.util.ArrayList;
+
+import android.media.session.MediaController;
+
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.Arm;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.Quaternion;
+import com.thalmic.myo.XDirection;
+import com.thalmic.myo.scanner.ScanActivity;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+
+public class Configuration extends ListActivity {
+	
+    String[] items = { "Music Player", "Video Player", "Google Maps", "Tinder", 
+ 		   "Camera" };
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+	    
+		TabHost tabs=(TabHost)findViewById(R.id.tabhost);
+		tabs.setup();
+				
+		TabHost.TabSpec spec;
+		
+		// Initialize a TabSpec for tab1 and add it to the TabHost
+		spec=tabs.newTabSpec("tag1");	//create new tab specification
+		spec.setContent(R.id.tab1);    //add tab view content
+		spec.setIndicator("Supported Installed Apps");    //put text on tab
+		tabs.addTab(spec);             //put tab in TabHost container
+        
+	    ArrayAdapter<String> aa = new ArrayAdapter<String>(
+		      this,
+		      android.R.layout.simple_list_item_1,     //Android supplied List item format
+		      items);
+	    setListAdapter(aa);    //connect ArrayAdapter to <ListView>
+	    
+		// Initialize a TabSpec for tab2 and add it to the TabHost
+		spec=tabs.newTabSpec("tag2");		//create new tab specification
+		spec.setContent(R.id.tab2);			//add view tab content
+		spec.setIndicator("Gestures");
+		tabs.addTab(spec);					//put tab in TabHost container
+		
+		// Start the BackgroundService to receive and handle Myo events.
+        startService(new Intent(this, BackgroundService.class));
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.configuration, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (R.id.action_scan == id) {
+            onScanActionSelected();
+            return true;
+        } else if (R.id.start_service == id) {
+        	startService(new Intent(this, BackgroundService.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onScanActionSelected() {
+        // Launch the ScanActivity to scan for Myos to connect to.
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
+    }
+    
+}
